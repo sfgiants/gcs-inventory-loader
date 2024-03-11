859f39f4a2f674018f4ea05710edbbb402667900
DarAmani Swift <d.swift510@gmail.com>
2024-03-11 16:05:31 -0700

    Revert "adding changes from https://github.com/sfgiants/gcs-inventory-loader/pull/1"
    This reverts commit 737ea308e896823df1df32407df604124738577b.

--- gcs_inventory_loader/cli/listen.py
+++ gcs_inventory_loader/cli/listen.py
@@ -19,7 +19,6 @@ import atexit
 from concurrent.futures import TimeoutError
 import json
 import logging
-from configparser import ConfigParser
 
 from google.api_core.exceptions import AlreadyExists
 from google.cloud import pubsub_v1 as pubsub
@@ -29,7 +28,7 @@ from google.cloud.pubsub_v1.subscriber.futures import StreamingPullFuture
 from gcs_inventory_loader.bq.output import BigQueryOutput
 from gcs_inventory_loader.bq.tables import TableDefinitions, get_table
 from gcs_inventory_loader.config import get_config
-from gcs_inventory_loader.thread import BoundedThreadPoolExecutor
+from gcs_inventory_loader.bq.client import get_bq_client
 
 LOG = logging.getLogger(__name__)
 
@@ -41,39 +40,36 @@ def listen_command() -> None:
     config = get_config()
     # Call this once to initialize the table.
     _ = BigQueryOutput(
-        get_table(TableDefinitions.INVENTORY, config.get("BIGQUERY", "INVENTORY_TABLE"))
-    )
+        get_table(TableDefinitions.INVENTORY,
+                  config.get("BIGQUERY", "INVENTORY_TABLE")))
 
     subscriber = pubsub.SubscriberClient()
-    topic_name = "projects/{}/topics/{}".format(
-        config.get("GCP", "PROJECT"), config.get("PUBSUB", "TOPIC_SHORT_NAME")
-    )
-    subscription_name = "projects/{}/subscriptions/{}".format(
-        config.get("GCP", "PROJECT"), config.get("PUBSUB", "SUBSCRIPTION_SHORT_NAME")
-    )
+    topic_name = 'projects/{}/topics/{}'.format(
+        config.get("GCP", "PROJECT"), config.get("PUBSUB", "TOPIC_SHORT_NAME"))
+    subscription_name = 'projects/{}/subscriptions/{}'.format(
+        config.get("GCP", "PROJECT"),
+        config.get("PUBSUB", "SUBSCRIPTION_SHORT_NAME"))
     LOG.info("Creating or adopting subscription {}.".format(subscription_name))
     try:
-        subscriber.create_subscription(
-            name=subscription_name, topic=topic_name, ack_deadline_seconds=60
-        )
+        subscriber.create_subscription(name=subscription_name,
+                                       topic=topic_name,
+                                       ack_deadline_seconds=60)
     except AlreadyExists:
         pass
 
     output = BigQueryOutput(
-        get_table(
-            TableDefinitions.INVENTORY, config.get("BIGQUERY", "INVENTORY_TABLE")
-        ),
-        False,
-    )
+        get_table(TableDefinitions.INVENTORY,
+                  config.get("BIGQUERY", "INVENTORY_TABLE")), False)
 
     def handle(message):
-        """Callback for handling new PubSub messages. Effectively, this just "partially applies"
-        the output stream above to unpack_and_insert.
+        """Callback for handling new PubSub messages. Effectively, this just
+        "partially applies" the output stream above to unpack_and_insert.
         """
         unpack_and_insert(output, message)
 
     def shutdown(sub_future: StreamingPullFuture) -> None:
-        """Close subscriptions and flush rows to BQ."""
+        """Close subscriptions and flush rows to BQ.
+        """
         LOG.info("Cancelling subscription pull.")
         sub_future.cancel()
         LOG.info("Flushing rows to BigQuery.")
@@ -91,12 +87,11 @@ def listen_command() -> None:
             try:
                 subscription_future.result(timeout=timeout)
             except TimeoutError:
-                LOG.debug(
-                    "No messages in {} seconds, flushing rows (if any).".format(timeout)
-                )
+                LOG.debug("No messages in {} seconds, flushing rows (if any).".
+                          format(timeout))
                 output.flush()
-            except:
-                LOG.info("Quitting...")
+            except Exception as e:
+                LOG.error(f"An exception occured: {e}\nShutting Down.")
                 break
 
 
@@ -105,43 +100,76 @@ def unpack_and_insert(output: BigQueryOutput, message: Message) -> None:
     a BigQueryOutput.
 
     Args:
-        output (BigQueryOutput): The output to use. In most cases, you will want to use a single
-        output object per program.
+        output (BigQueryOutput): The output to use. In most cases, you will
+        want to use a single output object per program.
         message (Message): The PubSub message.
     """
+    bq_client = get_bq_client()
+    config = get_config()
+    table = get_table(TableDefinitions.INVENTORY,
+                      config.get("BIGQUERY", "INVENTORY_TABLE"))
+    table_name = table.get_fully_qualified_name()
+
     try:
-        LOG.debug("Message data: \n---DATA---\n{}\n---DATA---".format(message.data))
+        LOG.debug("Message data: \n---DATA---\n{}\n---DATA---".format(
+            message.data))
 
         # Decode and deserialize
         message_string = bytes.decode(message.data, "UTF-8")
         object_info = json.loads(message_string)
-        if object_info.get("metadata"):
-            object_info["metadata"] = [
-                {"key": k, "value": v} for k, v in object_info["metadata"].items()
-            ]
+
+        LOG.debug(message)
+        LOG.debug(object_info)
 
         # Get important attributes
-        event_type = message.attributes["eventType"]
+        event_type = message.attributes['eventType']
         publish_time = message.publish_time.isoformat()
-        LOG.info(
-            "Got a message: {} {} {}".format(
-                publish_time,
-                event_type,
-                object_info["bucket"] + "/" + object_info["name"],
-            )
-        )
+        LOG.info("Got a message: {} {} {}".format(
+            publish_time, event_type,
+            object_info['bucket'] + "/" + object_info['name']))
 
         # For deletes, use the publish time to approximate deleted time
         if event_type == "OBJECT_DELETE":
             object_info["timeDeleted"] = publish_time
+            if object_info.get("metadata"):
+                object_info["metadata"] = [{
+                    "key": k,
+                    "value": v
+                } for k, v in object_info["metadata"].items()]
 
-        # Enqueue for writing
-        output.put(object_info)
+        if event_type == "OBJECT_METADATA_UPDATE":
+
+            def generate_structs(arr):
+                res = '['
+                for s in arr:
+                    res += "STRUCT(\"{key}\" as key, \"{value}\" as value),".format(  # noqa: E501
+                        key=s['key'], value=s['value'])
+                res = res[:-1]
+                res += ']'
+                return res
+
+            querytext = "UPDATE `{table_name}`\
+                SET metadata = {new_metadata}\
+                WHERE id = '{id}'".format(
+                table_name=table_name,
+                new_metadata=generate_structs([{
+                    "key": k,
+                    "value": v
+                } for k, v in object_info["metadata"].items()]),
+                id=object_info["id"])
+            LOG.info("Running query: \n%s", querytext)
+            query_job = bq_client.query(querytext)
+            LOG.info(query_job.result())
+        else:
+            # Enqueue for writing
+            output.put(object_info)
+
         message.ack()
 
-    except:
+    except Exception:
         LOG.exception(
-            "Error processing message! ---DATA---\n{}\n---DATA---".format(message.data)
-        )
-        # TODO: A retry / DLQ policy would be useful, if not already present by default.
+            "Error processing message! ---DATA---\n{}\n---DATA---".format(
+                message.data))
+        # TODO: A retry / DLQ policy would be useful, if not already present
+        # by default.
         message.nack()
